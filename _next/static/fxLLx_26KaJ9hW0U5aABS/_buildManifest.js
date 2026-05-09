7d82d7dbe501f17664c309e39dbf297f8944c826
Gabriel Rossanesi <gabrielrossanesi@outlook.com>
2026-05-09 16:34:20 -0300

    Deploy static site

---
+++ _next/static/fxLLx_26KaJ9hW0U5aABS/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Proposta-Bloom-Gifts-Franchise-Portal//_next/:path+",
+        "destination": "/Proposta-Bloom-Gifts-Franchise-Portal/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
